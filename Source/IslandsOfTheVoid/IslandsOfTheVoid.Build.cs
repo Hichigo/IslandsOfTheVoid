1733c64739c34e7d5c800a01f0820d5fe12e761a
hichigo <hichigo91@gmail.com>
2016-08-06 19:13:37 +0300

    work UI in c++

--- Source/IslandsOfTheVoid/IslandsOfTheVoid.Build.cs
+++ Source/IslandsOfTheVoid/IslandsOfTheVoid.Build.cs
@@ -6,6 +6,6 @@ public class IslandsOfTheVoid : ModuleRules
 {
 	public IslandsOfTheVoid(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
-	}
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG", "Slate", "SlateCore" });
+    }
 }
--- Source/IslandsOfTheVoid/IslandsOfTheVoid.h
+++ Source/IslandsOfTheVoid/IslandsOfTheVoid.h
@@ -3,7 +3,16 @@
 #ifndef __ISLANDSOFTHEVOID_H__
 #define __ISLANDSOFTHEVOID_H__
 
+// for user widget
+#include "Runtime/UMG/Public/UMG.h"
+#include "Runtime/UMG/Public/UMGStyle.h"
+#include "Runtime/UMG/Public/Slate/SObjectWidget.h"
+#include "Runtime/UMG/Public/IUMGModule.h"
+#include "Runtime/UMG/Public/Blueprint/UserWidget.h"
+
 #include "EngineMinimal.h"
+
+
 
 DECLARE_LOG_CATEGORY_EXTERN(LogIslandsOfTheVoid, Log, All);
 
